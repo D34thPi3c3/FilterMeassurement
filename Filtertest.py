0d5958b19e95d5cd5795f6b0e26af8de05aa52ff
Rafael  Eberle <rafaeleberle1994@gmail.com>
2019-01-30 17:57:40 +0100

    Add some GUI stuff

--- Filtertest.py
+++ Filtertest.py
@@ -9,20 +9,23 @@ Edit Date:
 """
 #Import Backend Stuff because otherwise it doesnt work on Raspberry
 import os
-os.environ['KIVY_GL_BACKEND'] = 'gl'
+import platform
+if platform.system() == 'Linux':
+    os.environ['KIVY_GL_BACKEN'] = 'gl'
+else:
+    os.environ['KIVY_GL_BACKEND'] = 'sdl2'
 
 #Import Stuff for Kivy (GUI_TOOL)
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.config import Config
-from kivy.core.window import Window
+#from kivy.core.window import Window
 
 #Import other Important stuff:
 #platform: Check if Linux or Windows (because I Programmed on Windows)
 #smbus: For I2C
 #time: For time things as delay
-import platform
 if platform.system() == 'Linux':
     import smbus
     bus = smbus.SMBus(1)       
@@ -41,6 +44,9 @@ Config.set('graphics', 'width', '800')
 Config.set('graphics', 'height', '480')
 Config.write()
 
+#Some Global Variable
+textinputselected = 'min_height'
+
 #On a Rasperry Py, use this setup
 #Window.fullscreen = True
 
@@ -53,7 +59,7 @@ Builder.load_string("""
 <StartScreen>:
     BoxLayout:
         padding: 10
-        spacing: 10
+        spacing: 10      
         orientation: 'horizontal'
         CustButton:
             text: 'Start Messung'
@@ -61,13 +67,248 @@ Builder.load_string("""
                 root.manager.transition.direction = 'left'
                 root.manager.transition.duration = 0.25
                 root.manager.current = 'filterpressuremeasurement'
-                
+            
+        BoxLayout:
+            orientation: 'vertical'
+            CustButton:   
+                text: 'Gemessene Filter'
+                on_press:
+                    root.manager.transition.direction = 'left'
+                    root.manager.transition.duration = 0.25
+                    root.manager.current = 'filtersummary'
+            CustButton:
+                text: 'Config ändern'
+                on_press:
+                    root.manager.transition.direction = 'left'
+                    root.manager.transition.duration = 0.25
+                    root.manager.current = 'config'
+<ConfigScreen>:
+    id: configscreen
+    min_height: min_height
+    max_height: max_height
+    FloatLayout:
+        ## Füll Höhe Einbabe
+        Label:
+            text: 'Füll Höhe:'
+            size_hint: 0.18, 0.1
+            pos: 0, 432
+            halign: 'right'
+        Label:
+            text: 'min'
+            size_hint: 0.075, 0.1 
+            pos: 130, 432 
+        TextInput:
+            id: min_height
+            text: '000'
+            size_hint: 0.1, 0.06
+            pos: 190, 440
+            multiline: False
+            on_text_validate: configscreen.textinput_selected('min_height')
+        CheckBox:
+            size_hint: 0.05, 0.07
+            pos: 260, 440
+            group: 'select_input'
+        Label:
+            text: 'max'
+            size_hint: 0.075, 0.1
+            pos: 290, 432
+        TextInput:
+            id: max_height
+            text: '004'
+            size_hint: 0.1, 0.06
+            pos: 350, 440
+            multiline: False
+            on_text_validate: configscreen.textinput_selected('max_height')
+        CheckBox:
+            size_hint: 0.05, 0.07
+            pos: 420, 440
+            group: 'select_input'
+            
+        ## Kraft eingabe
+        Label:
+            text: 'Kraft [N]:'
+            size_hint: 0.18, 0.1
+            pos: 0, 384
+            halign: 'right'
+        Label:
+            text: 'min'
+            size_hint: 0.075, 0.1 
+            pos: 130, 384
+        TextInput:
+            text: '000'
+            size_hint: 0.1, 0.06
+            pos: 190, 392
+            valign: 'middle'
+        Label:
+            text: 'max'
+            size_hint: 0.075, 0.1
+            pos: 290, 384
+        TextInput:
+            text: '004'
+            size_hint: 0.1, 0.06
+            pos: 350, 392
+        
+        ## Unterdruck Motor eingabe
+        Label:
+            text: 'Udruck Motor [mbar]:'
+            size_hint: 0.18, 0.1
+            pos: 0, 336
+            halign: 'right'
+        Label:
+            text: 'min'
+            size_hint: 0.075, 0.1 
+            pos: 130, 336
+        TextInput:
+            text: '000'
+            size_hint: 0.1, 0.06
+            pos: 190, 344
+            valign: 'middle'
+        Label:
+            text: 'max'
+            size_hint: 0.075, 0.1
+            pos: 290, 336
+        TextInput:
+            text: '004'
+            size_hint: 0.1, 0.06
+            pos: 350, 344
+        
+        ## Durchfluss eingabe
+        Label:
+            text: 'Durchfluss [l/min]:'
+            size_hint: 0.18, 0.1
+            pos: 0, 288
+            halign: 'right'
+        Label:
+            text: 'min'
+            size_hint: 0.075, 0.1 
+            pos: 130, 288
+        TextInput:
+            text: '000'
+            size_hint: 0.1, 0.06
+            pos: 190, 296
+            valign: 'middle'
+        Label:
+            text: 'max'
+            size_hint: 0.075, 0.1
+            pos: 290, 288
+        TextInput:
+            text: '004'
+            size_hint: 0.1, 0.06
+            pos: 350, 296
+        
+        ## Druckverlust eingabe
+        Label:
+            text: 'Druckverlust [mbar]:'
+            size_hint: 0.18, 0.1
+            pos: 0, 240
+            halign: 'right'
+        Label:
+            text: 'min'
+            size_hint: 0.075, 0.1 
+            pos: 130, 240
+        TextInput:
+            text: '000'
+            size_hint: 0.1, 0.06
+            pos: 190, 248
+            valign: 'middle'
+        CheckBox:
+            size_hint: 0.05, 0.07
+            pos: 260, 248
+            group: 'select_input'
+        Label:
+            text: 'max'
+            size_hint: 0.075, 0.1
+            pos: 290, 240
+        TextInput:
+            text: '004'
+            size_hint: 0.1, 0.06
+            pos: 350, 248
+        CheckBox:
+            size_hint: 0.05, 0.07
+            pos: 0, 248
+            group: 'select_input'
+            
+        ## Eingabe Feld
         CustButton:
-            text: 'Gemessene Filter'
+            text: '1'
+            size_hint: 0.1125, 0.1875
+            pos: 465, 360
+            on_press: configscreen.add_number('1')
+        CustButton:
+            text: '2'
+            size_hint: 0.1125, 0.1875
+            pos: 560, 360
+            on_press: configscreen.add_number('2')
+        CustButton:
+            text: '3'
+            size_hint: 0.1125, 0.1875
+            pos: 655, 360
+            on_press: configscreen.add_number('3')
+        CustButton:
+            text: '4'
+            size_hint: 0.1125, 0.1875
+            pos: 465, 265
+            on_press: configscreen.add_number('4')
+        CustButton:
+            text: '5'
+            size_hint: 0.1125, 0.1875
+            pos: 560, 265
+            on_press: configscreen.add_number('5')
+        CustButton:
+            text: '6'
+            size_hint: 0.1125, 0.1875
+            pos: 655, 265
+            on_press: configscreen.add_number('6')
+        CustButton:
+            text: '7'
+            size_hint: 0.1125, 0.1875
+            pos: 465, 170
+            on_press: configscreen.add_number('7')
+        CustButton:
+            text: '8'
+            size_hint: 0.1125, 0.1875
+            pos: 560, 170
+            on_press: configscreen.add_number('8')
+        CustButton:
+            text: '9'
+            size_hint: 0.1125, 0.1875
+            pos: 655, 170
+            on_press: configscreen.add_number('9')
+        CustButton:
+            text: ','
+            size_hint: 0.1125, 0.1875
+            pos: 465, 75
+            on_press: configscreen.add_number(',')
+        CustButton:
+            text: '0'
+            size_hint: 0.1125, 0.1875
+            pos: 560, 75
+            on_press: configscreen.add_number('0')
+        CustButton:
+            text: 'DEL'
+            size_hint: 0.1125, 0.1875
+            pos: 655, 75
+            on_press: configscreen.delete_number()
+            
+        CustButton:
+            text: 'Zurück und nicht speichern'
+            size_hint: 0.5, 0.1
+            pos: 0, 0
             on_press:
-                root.manager.transition.direction = 'left'
+                root.manager.transition.direction = 'right'
                 root.manager.transition.duration = 0.25
-                root.manager.current = 'filtersummary'
+                root.manager.current = 'startscreen'
+        CustButton: 
+            text: 'Zurück und speichern'
+            size_hint: 0.5, 0.1
+            pos: 400, 0
+            on_press:
+                root.manager.transition.direction = 'right'
+                root.manager.transition.duration = 0.25
+                root.manager.current = 'startscreen'
+                
+        
+                
 
 <SummaryScreen>:
     BoxLayout:
@@ -200,13 +441,8 @@ class SummaryScreen(Screen):
 class PressureMeasurementScreen(Screen):
     startmeassurement = False
     def start_meassurement(self):
-<<<<<<< HEAD
         if platform.system() == 'nux':
             bus.write_byte()
-=======
-        if platform.system() == 'Linux':
-            bus.write_byte(PRESSURE_MEAS_ADDRESS,)
->>>>>>> b94446759a7a6019e7a823fd7f728b4d81aab921
         else:    
             self.status.text = "Status: 100%"
             self.startmeassurement = True
@@ -224,6 +460,18 @@ class FlowMeasurementScreen(Screen):
 class EndMeasurementScreen(Screen):
     pass
 
+class ConfigScreen(Screen):
+    def textinput_selected(self, selectedti):
+        textinputselected = selectedti
+        self.add_number(textinputselected)
+    
+    def add_number(self, number):
+        self.min_height.text += number
+    
+    def delete_number(self):
+        self.min_height.text = self.min_height.text[:-1]
+        
+
 
 
 # The ScreenManager controls moving between screens
@@ -237,6 +485,7 @@ screen_manager.add_widget(PressureMeasurementScreen(
         name="filterpressuremeasurement"))
 screen_manager.add_widget(FlowMeasurementScreen(name="flowmeasurement"))
 screen_manager.add_widget(EndMeasurementScreen(name="endmeasurement"))
+screen_manager.add_widget(ConfigScreen(name='config'))
 
 #Build the whole App with all the logical Part
 class FilterTestApp(App):
